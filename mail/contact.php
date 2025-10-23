702cd0db29ebabf5a969c99f2cddb6806d194cf8
Cristian Vazquez <cris-uri199@hotmail.com>
2025-10-23 15:14:19 -0600

    Update contact email to ESDEFFNOTIFICACIONES@GMAIL.COM across all relevant pages and the contact form for consistency.

--- mail/contact.php
+++ mail/contact.php
@@ -9,7 +9,7 @@ $email = strip_tags(htmlspecialchars($_POST['email']));
 $m_Asunto = strip_tags(htmlspecialchars($_POST['Asunto']));
 $Message = strip_tags(htmlspecialchars($_POST['Message']));
 
-$to = "info@example.com"; // Change this email to your //
+$to = "ESDEFFNOTIFICACIONES@GMAIL.COM"; // Change this email to your //
 $Asunto = "$m_Asunto:  $name";
 $body = "You have received a new Message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\n\nEmail: $email\n\nAsunto: $m_Asunto\n\nMessage: $Message";
 $header = "From: $email";
